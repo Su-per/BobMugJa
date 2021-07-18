0d4c2e30f1a3575014e9599961da0f071901260a
이주원 <60950583+Su-per@users.noreply.github.com>
2021-07-18 13:10:51 +0900

    Update main.py
    학교 선택 기능 추가
    조회중인 학교 표시 기능 추가

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup as bs
 from PyQt5 import QtCore, QtGui, QtWidgets
-from datetime import datetime, date, timedelta
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from bs4 import BeautifulSoup as bs
+from datetime import datetime, timedelta
 import requests
 import calendar
 import re
+import sys
 
 
 class Ui_MainWindow(object):
 
+    SCHUL_NM = "광주소프트웨어마이스터고등학교"
+    ATPT_OFCDC_SC_CODE = "F10"  # 교육청 코드
+    SD_SCHUL_CODE = "7380292"  # 학교 코드
     yesmorrow: int = 0  # 내일이면 +1, 어제면 -1
     weekday_list = [" (월)", " (화)", " (수)", " (목)", " (금)", " (토)", " (일)"]
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(970, 599)
+        MainWindow.resize(970, 644)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
@@ -49,6 +55,9 @@ class Ui_MainWindow(object):
         self.label.setFont(font)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(800, 570, 141, 41))
+        self.pushButton_3.setObjectName("pushButton_3")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 970, 26))
@@ -57,12 +66,35 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.statusbar.showMessage(self.SCHUL_NM)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.dialog = QDialog()
+        self.dialog.setWindowTitle("학교 선택")
+        self.dialog.setWindowModality(Qt.ApplicationModal)
+        self.dialog.resize(600, 360)
+        self.pushButton_search = QtWidgets.QPushButton("학교 검색", self.dialog)
+        self.pushButton_search.setGeometry(QtCore.QRect(440, 20, 121, 51))
+        self.pushButton_search.setObjectName("pushButton")
+        self.lineEdit_search = QtWidgets.QLineEdit(self.dialog)
+        self.lineEdit_search.setGeometry(QtCore.QRect(20, 20, 411, 51))
+        self.lineEdit_search.setObjectName("lineEdit")
+        self.listWidget_search = QtWidgets.QListWidget(self.dialog)
+        self.listWidget_search.setGeometry(QtCore.QRect(20, 80, 411, 192))
+        self.listWidget_search.setObjectName("listWidget")
+        self.pushButton_2_search = QtWidgets.QPushButton("학교 선택", self.dialog)
+        self.pushButton_2_search.setGeometry(QtCore.QRect(440, 80, 121, 41))
+        self.pushButton_2_search.setObjectName("pushButton_2")
+
+        self.pushButton_search.clicked.connect(self.search_school)
+        self.pushButton_2_search.clicked.connect(self.selected_school)
+        self.lineEdit_search.textChanged.connect(self.get_school_list)
 
         self.pushButton.clicked.connect(self.set_day_yesterday)
         self.pushButton_2.clicked.connect(self.set_day_tomorrow)
+        self.pushButton_3.clicked.connect(self.select_school)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -73,8 +105,47 @@ class Ui_MainWindow(object):
         self.groupBox_2.setTitle(_translate("MainWindow", "중식"))
         self.groupBox_3.setTitle(_translate("MainWindow", "석식"))
         self.label.setText(_translate("MainWindow", "날짜"))
+        self.pushButton_3.setText(_translate("MainWindow", "학교 선택"))
         self.set_date()
         self.set_meal()
+
+    def select_school(self):
+        self.dialog.show()
+
+    def search_school(self):
+        self.get_school_list()
+
+    def selected_school(self):
+        url = "https://open.neis.go.kr/hub/schoolInfo?SCHUL_NM="
+        try:
+            self.SCHUL_NM = self.listWidget_search.currentItem().text()
+        except:
+            return
+        url += self.SCHUL_NM
+
+        response = requests.get(url).text
+
+        index = response.find("ATPT_OFCDC_SC_CODE")
+        self.ATPT_OFCDC_SC_CODE = response[index + 28 : index + 31]
+
+        index = response.find("SD_SCHUL_CODE")
+        self.SD_SCHUL_CODE = response[index + 23 : index + 30]
+
+        self.set_meal()
+        self.dialog.close()
+
+        self.statusbar.showMessage(self.SCHUL_NM)
+
+    def get_school_list(self):
+        url = "https://open.neis.go.kr/hub/schoolInfo?SCHUL_NM="
+        self.listWidget_search.clear()
+        response = requests.get(url + self.lineEdit_search.text()).text
+        soup = bs(response, "lxml-xml")
+        res = soup.findAll(text=True)
+        for i in range(2, len(res)):
+            if "고등학교" in res[i] or "중학교" in res[i] or "초등학교" in res[i]:
+                if res[i - 2].isdigit():
+                    self.listWidget_search.addItem(res[i])
 
     def set_day_yesterday(self):
         self.yesmorrow -= 1
@@ -99,9 +170,10 @@ class Ui_MainWindow(object):
         list_widgets = [self.listWidget, self.listWidget_2, self.listWidget_3]
         date = self.get_date().replace("-", "")
         for i in range(1, 4):
+
             url = "https://open.neis.go.kr/hub/mealServiceDietInfo?"  # 기본 URL
-            url += "ATPT_OFCDC_SC_CODE=F10&"  # 교육청코드
-            url += "SD_SCHUL_CODE=7380292&"  # 학교코드
+            url += "ATPT_OFCDC_SC_CODE=" + self.ATPT_OFCDC_SC_CODE + "&"  # 교육청코드
+            url += "SD_SCHUL_CODE=" + self.SD_SCHUL_CODE + "&"  # 학교코드
             url += "MMEAL_SC_CODE=" + str(i) + "&"  # 1, 2, 3 : 아침, 점심, 저녁
             url += "MLSV_YMD=" + date  # 날짜
 
@@ -128,8 +200,6 @@ class Ui_MainWindow(object):
 
 
 if __name__ == "__main__":
-    import sys
-
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
